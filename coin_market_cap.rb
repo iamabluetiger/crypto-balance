8e5e70be6f1167cbb9be7f1fd05af17b00496baf
ten <ten@sentbe.com>
2018-06-13 13:50:08 +0900

    feat(cmc): Create coinmarketcap class
    1. Get coinmarketcap id from symbol
    2. Get cionmarketcap price follow by currency

---
+++ coin_market_cap.rb
@@ -0,0 +1,33 @@
+require 'json'
+require 'net/http'
+
+class CoinMarketCap
+  CMC_LIST = JSON(Net::HTTP.get_response(URI('https://api.coinmarketcap.com/v2/listings/')).body)["data"]
+  attr_reader :symbol, :id, :price, :currency
+
+  def initialize(symbol, currency)
+    @symbol = symbol
+    @currency = currency
+
+    @id = get_id
+    @price = get_price
+  end
+
+  private
+
+  def get_id
+    data = CMC_LIST.find { |list| list["symbol"] == symbol }
+    data ? data["id"] : nil
+  end
+
+  def get_price
+    return 0 unless id
+    get_data['quotes'][currency]['price']
+  end
+
+  def get_data
+    return nil unless id
+    @data ||= JSON(Net::HTTP.get_response(URI("https://api.coinmarketcap.com/v2/ticker/#{id}/?convert=#{currency}")).body)['data']
+  end
+
+end
